f0d187b5dac29d3b04e333de21a8fccb5aebe2a8
tahmidyouthlink <t.rahman@youthlink.tech>
2025-06-16 22:23:04 +0600

    changed code for 30 min inactivity

--- app/(withLayout)/layout.js
+++ app/(withLayout)/layout.js
@@ -1,12 +1,12 @@
 import React from "react";
 import DashboardNavbar from "@/app/components/layout/DashboardNavbar/DashboardNavbar";
 import SideNavbar from "@/app/components/layout/SideNavbar/SideNavbar";
-import InactivityHandlerWrapper from "../components/inactivity/InactivityHandlerWrapper";
+// import InactivityHandlerWrapper from "../components/inactivity/InactivityHandlerWrapper";
 
 const Layout = ({ children }) => {
   return (
     <div>
-      <InactivityHandlerWrapper />
+      {/* <InactivityHandlerWrapper /> */}
       {/* Only render sidebar on large devices */}
       <div className="inset-y-0 hidden flex-col xl:flex">
         <SideNavbar />
