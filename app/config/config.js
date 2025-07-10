b9b9c32278c661cc1f79d42129ed25678bc6a973
tahmidyouthlink <t.rahman@youthlink.tech>
2025-07-10 21:36:27 +0600

    feat(config) : changed backend url to cloud run

--- app/config/config.js
+++ app/config/config.js
@@ -1,3 +1,3 @@
 export const WEBSITE_NAME = "PoshaX";
-// export const BACKEND_URL = "https://fc-backend-664306765395.asia-south1.run.app";
-export const BACKEND_URL = "http://localhost:5000";
+export const BACKEND_URL = "https://fc-backend-664306765395.asia-south1.run.app";
+// export const BACKEND_URL = "http://localhost:5000";
